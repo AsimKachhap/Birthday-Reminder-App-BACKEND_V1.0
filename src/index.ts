1b2eed3fd9a24fbc1d847ce850ac4b8d96141e48
unknown <asim26k@gmail.com>
2023-04-21 07:12:55 +0530

    SUCCESSFULLY connected with MONGODB SERVERS.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import express from "express";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 const app = express();
+
+app.use(morgan("dev"));
+
+dotenv.config();
+
+(async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION, {
+      useNewUrlParser: true,
+    } as mongoose.ConnectOptions);
+    console.log("CONNECTED TO MONGO DB SERVER");
+  } catch (error) {
+    console.log("MONGO DB CONNECTION ERROR");
+    console.log(error);
+  }
+})();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("You are on HomePage");
